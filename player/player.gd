cc7faf843b352c1acaf03bbbbe1be416cd112e7f
Hashir Shahzad <hashir447079@gmail.com>
2024-11-16 10:35:36 +0500

    idk

--- player/player.gd
+++ player/player.gd
@@ -1,35 +1,45 @@
 extends CharacterBody3D
+class_name Player3D
 
+# <========================== Variables ====================================>
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var speed : float = 5.0
+var jump_velocity : float = 4.5
+var direction : Vector3
 
 var facing: Vector3:
 	get: return global_transform.basis.z
 	
+	
+# <========================== Functions ========================================>
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("dash"):
+		self.velocity = direction * 100
+		print(direction)
+		
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 		
-	if velocity.x > 0:
-		$Sprite3D.flip_h = true
-	elif velocity.x < 0:
-		$Sprite3D.flip_h = false
-
+	flip_sprite($Sprite3D)
 	move_and_slide()
+
+func flip_sprite(sprite : Sprite3D):
+	if velocity.x > 0:
+		sprite.flip_h = true
+	elif velocity.x < 0:
+		sprite.flip_h = false
