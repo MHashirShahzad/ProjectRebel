6f589067cb3deab1fc76b50732b4c8727bc4c3a1
Hashir Shahzad <hashir447079@gmail.com>
2024-11-19 16:31:14 +0500

    Added grass

--- player/shadow.gd
+++ player/shadow.gd
@@ -1,11 +1,16 @@
 extends Sprite3D
-@onready var shadow_cast: RayCast3D = $"../ShadowCast"
 
+@onready var shadow_cast: RayCast3D = $"../ShadowCast"
+@onready var player: Player3D = $".."
+
+@export var y_dist : float = 0.1
 
 func _process(delta: float) -> void:
+	#if player.is_on_floor():
+		#return
 	if shadow_cast.is_colliding():
 		self.modulate.a = 1
 		self.global_position = shadow_cast.get_collision_point()
-		self.global_position.y += 0.05
+		self.global_position.y += y_dist
 	else:
 		self.modulate.a = 0
